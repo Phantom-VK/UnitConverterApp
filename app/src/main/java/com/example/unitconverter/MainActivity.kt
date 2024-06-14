2cf13cf4cae133101d0ab04c75e39eecdf7545da
Phantom-VK <vikramadityakhupse@gmail.com>
2024-06-14 20:29:25 +0530

    Added some composable, initial UI of app using jetpack compose.

--- app/src/main/java/com/example/unitconverter/MainActivity.kt
+++ app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -1,15 +1,33 @@
 package com.example.unitconverter
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.unitconverter.ui.theme.UnitConverterTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +40,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    UnitConverter()
                 }
             }
         }
@@ -30,17 +48,55 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun UnitConverter(){
+    Column(
+    modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+
+    ) {
+        Text(text = "Unit Converter")
+        //Spacer to give space
+        Spacer(modifier = Modifier.height(16.dp))
+        
+        //TextField to input value
+        OutlinedTextField(value = "", onValueChange ={
+        /*Here we can put what will happen when outlinedtf changes*/}
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Row() {
+            val context = LocalContext.current
+            //Button to select initial unit to convert from
+            Box{
+
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "From")
+                    Icon(imageVector = Icons.Default.ArrowDropDown, "Unit From DropDown Icon")
+                }
+            }
+            //Spacer to give space between two buttons
+            Spacer(modifier = Modifier.width(80.dp))
+            //Button to select unit to convert to
+            Box {
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "To")
+                    Icon(imageVector = Icons.Default.ArrowDropDown, "Unit From DropDown Icon")
+                }
+            }
+        }
+        Row {
+            //Text showing result
+            Text(text = "Result: ")
+        }
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun UnitConverterPreview() {
     UnitConverterTheme {
-        Greeting("Android")
+        UnitConverter()
     }
 }
