426a4d83b7963cb4c017e29f1090d4e22ff0e7c8
Phantom-VK <vikramadityakhupse@gmail.com>
2024-06-16 11:43:06 +0530

    Added logic/function to convert units.(Not Working)

--- app/src/main/java/com/example/unitconverter/MainActivity.kt
+++ app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -32,11 +32,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.unitconverter.ui.theme.UnitConverterTheme
+import kotlin.math.roundToInt
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -77,9 +80,20 @@ fun UnitConverter(){
         mutableStateOf(false)
     }
     val conversionFactor = remember {
+        mutableDoubleStateOf(1.00)
+    }
+    val oConversionFactor = remember {
         mutableDoubleStateOf(100.00)
     }
 
+    //Function to convert units
+    fun convert(){
+
+        val inputValueDouble = inputValue.toDoubleOrNull()?: 0.0
+        val result = (inputValueDouble * conversionFactor.doubleValue * 100.0/oConversionFactor.doubleValue).roundToInt()/100
+        outputValue = result.toString()
+
+    }
     Column(
     modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
@@ -116,26 +130,41 @@ fun UnitConverter(){
                 }) {
                     DropdownMenuItem(text = { Text(text = "Centimeters")}, onClick = { inputUnit = "Centimeters"
                         iExpanded = false
-                        inputButtonText = inputUnit})
+                        inputButtonText = inputUnit
+                        conversionFactor.doubleValue = 100.00
+                        convert()
+
+
+                    })
                     DropdownMenuItem(text = { Text(text = "Meters")}, onClick = { inputUnit = "Meters"
                         iExpanded = false
                         inputButtonText = inputUnit
+                        conversionFactor.doubleValue = 1.00
+                        convert()
                         })
                     DropdownMenuItem(text = { Text(text = "Kilometers")}, onClick = { inputUnit = "Kilometers"
                         iExpanded = false
                         inputButtonText = inputUnit
+                        conversionFactor.doubleValue = 0.001
+                        convert()
                          })
                     DropdownMenuItem(text = { Text(text = "Millimeters")}, onClick = { inputUnit = "Millimeters"
                         iExpanded = false
                         inputButtonText = inputUnit
+                        conversionFactor.doubleValue = 1000.00
+                        convert()
                          })
                     DropdownMenuItem(text = { Text(text = "Decimeters")}, onClick = {inputUnit = "Decimeters"
                         iExpanded = false
                         inputButtonText = inputUnit
+                        conversionFactor.doubleValue = 10.00
+                        convert()
                          })
                     DropdownMenuItem(text = { Text(text = "Feet")}, onClick = { inputUnit = "Feet"
                         iExpanded = false
                         inputButtonText = inputUnit
+                        conversionFactor.doubleValue = 3.28084
+                        convert()
                          })
                 }
             }
@@ -160,39 +189,53 @@ fun UnitConverter(){
                     DropdownMenuItem(text = { Text(text = "Centimeters")}, onClick = { outputUnit = "Centimeters"
                         oExpanded = false
                         outputUnitButtonText = outputUnit
+                        oConversionFactor.doubleValue = 100.00
+                        convert()
 
                     })
                     DropdownMenuItem(text = { Text(text = "Meters")}, onClick = { outputUnit = "Meters"
                         oExpanded = false
                         outputUnitButtonText = outputUnit
+                        oConversionFactor.doubleValue = 1.00
+                        convert()
 
                     })
                     DropdownMenuItem(text = { Text(text = "Kilometers")}, onClick = { outputUnit = "Kilometers"
                         oExpanded = false
                         outputUnitButtonText = outputUnit
+                        oConversionFactor.doubleValue = 0.001
+                        convert()
 
                     })
                     DropdownMenuItem(text = { Text(text = "Millimeters")}, onClick = { outputUnit = "Millimeters"
                         oExpanded = false
                         outputUnitButtonText = outputUnit
+                        oConversionFactor.doubleValue = 1000.00
+                        convert()
                     })
                     DropdownMenuItem(text = { Text(text = "Decimeters")}, onClick = {outputUnit = "Decimeters"
                         oExpanded = false
                         outputUnitButtonText = outputUnit
+                        oConversionFactor.doubleValue = 10.00
+                        convert()
 
                     })
                     DropdownMenuItem(text = { Text(text = "Feet")}, onClick = { outputUnit = "Feet"
                         oExpanded = false
                         outputUnitButtonText = outputUnit
+                        oConversionFactor.doubleValue =  3.28084
+                        convert()
                         })
                 }
                 }
+
             }
-        }
         Row {
             //Text showing result
-            Text(text = "Result: ")
+            Text(text = "Result: $outputValue  ", color = Color.DarkGray, fontFamily = FontFamily.Serif)
         }
+        }
+
 
     }
 
